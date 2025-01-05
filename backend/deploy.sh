addebec95cd45cf492e92cae54dd274db339d9b0
loganphillips792 <loganphillips792@gmail.com>
2025-01-04 22:55:50 -0500

    add image tag

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,11 +2,12 @@
 
 # Variables
 IMAGE_NAME="dockedupstream/digitaloceanpythondevelopment"
+IMAGE_TAG="latest"
 CONTAINER_NAME="my_app"
 PORT=8080  # Change based on your app's configuration
 
 # Pull the latest image
-docker pull $IMAGE_NAME
+docker pull $IMAGE_NAME:$IMAGE_TAG
 
 # Stop and remove the current container if it exists
 docker rm -f $CONTAINER_NAME || true
